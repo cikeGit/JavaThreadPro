00ab4fcd73216528a69649b28caf7349e2f32e7a
cike <1219259875@qq.com>
2017-08-25 22:04:51 +0800

    init

---
+++ threadTrain/src/main/java/zhaopx/ThreadTraining/FirstTrainTest.java
@@ -0,0 +1,13 @@
+package zhaopx.ThreadTraining;
+
+/**
+ * Created by oliver on 2017/8/25.
+ */
+public class FirstTrainTest {
+
+    public  static  void  main(String[] args){
+        System.out.print(" 大道之间 ");
+    }
+
+
+}
